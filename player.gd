fe4e5f75868c9f36ee2923112a02b9e95e58f6da
Sebs <s3bastianporras@gmail.com>
2026-05-08 00:47:52 -0500

    integracion de poder direccionar la creacion del block al estar quieto

--- player.gd
+++ player.gd
@@ -7,9 +7,12 @@ var move_speed = 120
 
 var last_direction = Vector2.RIGHT
 
+var hold_timer = 0.0
+var hold_delay = 0.1
+
 @onready var ice_block_scene = preload("res://block.tscn")
 
-func _ready(): 
+func _ready():
 
 	target_position = global_position
 
@@ -29,40 +32,59 @@ func _physics_process(delta):
 
 		return
 
-	var direction = Vector2.ZERO
+	var input_direction = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
 
-		direction = Vector2.RIGHT
-		last_direction = direction
+		input_direction = Vector2.RIGHT
 
 	elif Input.is_action_pressed("ui_left"):
 
-		direction = Vector2.LEFT
-		last_direction = direction
+		input_direction = Vector2.LEFT
 
 	elif Input.is_action_pressed("ui_down"):
 
-		direction = Vector2.DOWN
-		last_direction = direction
+		input_direction = Vector2.DOWN
 
 	elif Input.is_action_pressed("ui_up"):
 
-		direction = Vector2.UP
-		last_direction = direction
+		input_direction = Vector2.UP
 
-	if direction != Vector2.ZERO:
+	# SI PRESIONA UNA DIRECCIÓN
+	if input_direction != Vector2.ZERO:
 
-		var movement = direction * grid_size
+		# CAMBIAR DIRECCIÓN SIN MOVER
+		if input_direction != last_direction:
 
-		if not test_move(transform, movement):
+			last_direction = input_direction
+			hold_timer = 0
+			return
 
-			target_position += movement
-			moving = true
+		# CONTAR TIEMPO MANTENIENDO
+		hold_timer += delta
 
+		# MOVER SI YA PASÓ EL DELAY
+		if hold_timer >= hold_delay:
+
+			move_player(input_direction)
+
+	else:
+
+		hold_timer = 0
+
+	# CREAR BLOQUE
 	if Input.is_action_just_pressed("ui_accept"):
 
 		create_ice_block()
+
+func move_player(direction):
+
+	var movement = direction * grid_size
+
+	if not test_move(transform, movement):
+
+		target_position += movement
+		moving = true
 
 func create_ice_block():
 
@@ -75,3 +97,4 @@ func create_ice_block():
 	block.global_position = block_position
 
 	get_parent().add_child(block)
+  
